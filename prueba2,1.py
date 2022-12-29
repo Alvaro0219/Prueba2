90aa1f4a2136e89bb8f2ad456afb2cee29411803
Alvaro Conti <alvaroconti05@gmail.com>
2022-12-29 19:12:07 -0300

    Agregando gitignore

--- prueba2,1.py
+++
@@ -1,7 +0,0 @@
-
-"""Hallar la superficie de un cuadrado conociendo el valor de un lado."""
-
-lado=int (input("Ingrese la medida del lado del cuadrado:"))
-superficie=lado*lado
-print("La superficie del cuadrado es")
-print(superficie)
